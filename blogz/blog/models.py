4deb5e9c79d8ba1c55cb10cdbc612db22376f6e0
Rus <valiev.ruslan@gmail.com>
2021-07-22 21:07:10 +0500

    'update_2'

--- blogz/blog/models.py
+++ blogz/blog/models.py
@@ -1,6 +1,13 @@
-from django.db import models 
+from django.db import models
 from django.utils import timezone 
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):  
+    def get_queryset(self):  
+        return super(PublishedManager,  
+		     self).get_queryset().filter(status='published')
 
 class Post(models.Model): 
     STATUS_CHOICES = (
@@ -19,7 +26,13 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True) 
     status = models.CharField(max_length=10, 
                               choices=STATUS_CHOICES, 
-                              default='draft') 
+                              default='draft')
+    def get_absolute_url(self):  
+        return reverse('blog:post_detail',  
+		       args=[self.publish.year,  
+		       self.publish.month,  
+		       self.publish.day,  
+		       self.slug])
 
     class Meta: 
         ordering = ('-publish',) 
