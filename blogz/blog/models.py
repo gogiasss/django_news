b4e47ee48f13f36c933062fa62976e5b748a806d
Rus <valiev.ruslan@gmail.com>
2021-07-22 22:56:12 +0500

    update main page

--- blogz/blog/models.py
+++ blogz/blog/models.py
@@ -27,12 +27,17 @@ class Post(models.Model):
     status = models.CharField(max_length=10, 
                               choices=STATUS_CHOICES, 
                               default='draft')
+
+    objects = models.Manager()   
+    published = PublishedManager() 
+
     def get_absolute_url(self):  
         return reverse('blog:post_detail',  
 		       args=[self.publish.year,  
 		       self.publish.month,  
 		       self.publish.day,  
 		       self.slug])
+
 
     class Meta: 
         ordering = ('-publish',) 
