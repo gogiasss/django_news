403f040aa30b6268528f56f778474ab3b1d53932
Rus <valiev.ruslan@gmail.com>
2021-07-23 10:03:27 +0500

    check

--- blogz/blog/views.py
+++ blogz/blog/views.py
@@ -5,15 +5,13 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):  
     object_list = Post.published.all()  
-    paginator = Paginator(object_list, 4)  # 3 поста на каждой странице  
+    paginator = Paginator(object_list, 4)  # 4 post on page
     page = request.GET.get('page')  
     try:  
         posts = paginator.page(page)  
-    except PageNotAnInteger:  
-        # Если страница не является целым числом, поставим первую страницу  
+    except PageNotAnInteger:         
         posts = paginator.page(1)  
-    except EmptyPage:  
-        # Если страница больше максимальной, доставить последнюю страницу результатов  
+    except EmptyPage:          
         posts = paginator.page(paginator.num_pages)  
     return render(request,  
 	          'blog/post/list.html',  
